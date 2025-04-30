3026119373292d482fdcabfd4547fb7e41d69b17
ehsan zarghalami <ehsan.zarghalami@gmail.com>
2025-04-30 17:17:03 +0330

    implement data layer dependency module

---
+++ app/src/main/java/kb/ehsan/barook/di/DependencyQualifier.kt
@@ -0,0 +1,11 @@
+package kb.ehsan.barook.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class BaseUrl
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class BaseImageUrl
